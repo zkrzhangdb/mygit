dabfd3f8d6edb03b73ad00fba8b921e733d97f4c
AB055474 <AB055474zhangdongbin@ab-insurance.com>
2019-10-29 15:44:11 +0800

    branchone commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,6 @@
 public class GitTest {
     public  static  void  main(String[] args){
              System.out.println("创建git。。。99");
+             System.out.println("branchone-jt-8888");
          }
 }
